b267819feb1a8438750e4b3169496b32cc9fed64
borislaw <borislawpanov@gmail.com>
2025-07-13 10:48:42 +0000

    fix

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 export const platformUrl = '/login';
 
-export const contactPhone = '+49 30 12345678';
-export const contactEmail = 'info@woodmont-trading.com';
+export const contactPhone = '+49 30 42143662';
+export const contactEmail = 'info@woodmont.ai';
 export const contactAddress = 'London, UK';
 
 export const goToPlatform = () => (window.location.href = platformUrl);
