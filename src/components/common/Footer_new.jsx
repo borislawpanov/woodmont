f1496195e0a1898e3fed365425fe025e57678141
borislaw <borislawpanov@gmail.com>
2025-07-18 10:36:03 +0000

    deploy

--- src/components/common/Footer_new.jsx
+++ src/components/common/Footer_new.jsx
@@ -34,8 +34,8 @@ const Footer = () => {
     { name: t('navigation.faq'), path: "/learning/faq" },
     { name: t('navigation.plans'), path: "/plans" },
     { name: t('navigation.about'), path: "/about" },
-    { name: t('navigation.legalInfo'), path: "/legal-info" },
-    { name: t('navigation.imprint'), path: "/imprint" },
+    { name: t('navigation.legal'), path: "/legal-info" },
+    { name: t('navigation.impressum'), path: "/imprint" },
     { name: t('navigation.contact'), path: "/contact" },
   ];
 
